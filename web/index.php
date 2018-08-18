9acd0fb4223a01687acee17c8fc0a9f1e76b92c1
Tagir <gtagir11@yandex.ru>
2018-08-18 12:12:57 +0500

    UPD

--- web/index.php
+++ web/index.php
@@ -10,16 +10,33 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+  return "Hello world";
+});
+
+$app->post('/bot', function() use($app) {
+  $data = json_decode(file_get_contents('php://input'));
+
+  if(!$data)
+    return "nioh";
+  
+  if( $data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation')
+    return 'nioh';
+
+  switch($data->type){
+    case 'confirmation':
+      return getenv('VK_SECRET_TOKEN');
+      break;
+    
+    case 'message_new':
+
+
+
+      break;
+  }
+  return "nioh";
 });
 
 $app->run();

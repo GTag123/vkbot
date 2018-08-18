bd8d10d35f51e801adfbaf82f0da4b7f8329f2b5
Tagir <gtagir11@yandex.ru>
2018-08-18 17:19:46 +0500

    UDP7

--- web/index.php
+++ web/index.php
@@ -36,7 +36,7 @@ $app->post('/bot', function() use($app) {
         'user_id'=> '239188570',
         'message'=>'Тест',
         'access_token' -> getenv('VK_TOKEN'),
-        'v'=> '5.69'
+        'v'=> '5.80'
       );
 
       file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));

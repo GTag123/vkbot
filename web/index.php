7b3bf1dff4dd66194d4a527b2ad1898823eac4dd
Tagir <gtagir11@yandex.ru>
2018-08-23 17:07:05 +0500

    push4

--- web/index.php
+++ web/index.php
@@ -33,7 +33,7 @@ $app->post('/bot', function() use($app) {
     case 'message_new':
       if ( $data->object->text == 'Ку' || $data->object->text == 'ку' ){
         $request_params = array(
-          'chat_id' => "{$data->object->chat_id}",
+          'peer_id' => 2000000000 + $data->object->chat_id,
           // 'user_id' => "{$data->object->from_id}",
           'message'=>'Привет',
           'access_token' => '18d28ce6782d1c964c4bac21f4fd054378c65e739089d1bcae856947b32657436f5c2d06faa5179289e08',

58695f6ee6392a86b3027744f6612d0a7efc9fdc
Tagir <gtagir11@yandex.ru>
2018-08-24 15:29:44 +0500

    anekdots

--- web/index.php
+++ web/index.php
@@ -64,7 +64,7 @@ $app->post('/bot', function() use($app) {
         $rand = $anekdots[rand(0, count($anekdots)-1)];
         $request_params = array(
           'user_id' => "{$data->object->from_id}",
-          'message'=> "{$rand}",
+          'message'=> $anekdots[0],
           'access_token' => '18d28ce6782d1c964c4bac21f4fd054378c65e739089d1bcae856947b32657436f5c2d06faa5179289e08',
           'v' => '5.80'
         );

edf94bf0bd02fe10f662f535bc5701a54cce9f54
Tagir <gtagir11@yandex.ru>
2018-08-28 22:23:27 +0500

    123

--- web/index.php
+++ web/index.php
@@ -33,7 +33,6 @@ $app->post('/bot', function() use($app) {
     case 'message_new':
       $request_params = array(
         'peer_id' => "{$data->object->peer_id}",
-        'message'=>'null',
         'access_token' => '18d28ce6782d1c964c4bac21f4fd054378c65e739089d1bcae856947b32657436f5c2d06faa5179289e08',
         'v' => '5.80'
       );

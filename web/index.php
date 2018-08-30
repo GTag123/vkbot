a331c2c265f18bff0fd9e467f26410d9caac46c7
Tagir <gtagir11@yandex.ru>
2018-08-30 13:23:06 +0500

    yoe

--- web/index.php
+++ web/index.php
@@ -13,7 +13,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  return "Hello world";
+  return "тагир дебил";
 });
 $app->post('/bot', function() use($app) {
   $data = json_decode(file_get_contents('php://input'));

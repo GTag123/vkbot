ac9d4d4dbd5ba4da9476b95a043c285873b3c16f
Tagir <gtagir11@yandex.ru>
2018-11-24 19:26:13 +0500

    anekdot

--- web/index.php
+++ web/index.php
@@ -91,9 +91,8 @@ $app->post('/bot', function() use($app) {
           'key' => "trnsl.1.1.20181124T122415Z.1606631df5f130cf.d1a65715f0dd3d20cedfa225a1442f4d7ecc2082",
           'text' => $split2[1],
           'lang' => $lang[(int)$split2[0]-1]
-        ))), true)['text'][0];
+        ))), true)['lang'][0];}
       }
-    }
 
       elseif ( $split[0] == '!анекдот' ){
         $request_params['message'] = json_decode(file_get_contents('http://rzhunemogu.ru/RandJSON.aspx?CType=1'), true)["content"][0];

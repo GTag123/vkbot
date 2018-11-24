6e53e15c4a5ba544f34514a93837f52dcb5520be
Tagir <gtagir11@yandex.ru>
2018-11-24 19:05:06 +0500

    anekdot

--- web/index.php
+++ web/index.php
@@ -97,7 +97,7 @@ $app->post('/bot', function() use($app) {
 
       elseif ( $split[0] == '!анекдот' ){
         $request_params['message'] = json_decode(file_get_contents('http://rzhunemogu.ru/RandJSON.aspx?CType=1'))["content"];
-      
+      }
       /* elseif ( $split[0] == '!бд'){
         $query = pg_query($con, "INSERT INTO messages (user_id, message) VALUES (22112, 'прив');");
         $request_params['message'] = 'gg';
